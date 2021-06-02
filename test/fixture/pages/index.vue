a9ecbbc8bbc09c31bb8cd7535f1b7370fa35b6ff
Danny Atthaya <dannyatthaya9@gmail.com>
2021-06-02 15:16:15 +0700

    Initial commit

---
+++ test/fixture/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>@badaso/nuxt</h1>
+    <h2>$badaso.state</h2>
+    <pre>{{ JSON.stringify($badaso.state) }}</pre>
+    <h2>options.url</h2>
+    <pre>{{ $badaso.$http._defaults.prefixUrl }}</pre>
+
+    <pre>{{ posts }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: {}
+    }
+  },
+  async fetch() {
+    this.posts = await this.$badaso.$category.browse({
+      slug: 'terapkan-sgds-perusahaan-otomotif-hemat-biaya-produksi-70',
+      page: 1
+    });
+  },
+}
+</script>
